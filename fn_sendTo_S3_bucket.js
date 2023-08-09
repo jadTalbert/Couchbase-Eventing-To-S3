342bc0443c14d71389a645d1eb08ee8b5a730259
Jad Talbert <jad.talbert@gmail.com>
2023-08-09 10:10:30 -0400

    cleaning up function and adding more comments

--- fn_sendTo_S3_bucket.js
+++ fn_sendTo_S3_bucket.js
@@ -1,31 +1,26 @@
-# Couchbase-Eventing-To-S3
-
 function OnUpdate(doc, meta) {
-
+    /*set up the request object for the HTTPS request to S3*/
     var request = {
-        path: "/beta/test-pru-copy/" + meta.id + ".json",
+        path: "/beta/test-pru-copy/" + meta.id + ".json", /*S3 bucket path*/
         body: doc,
         headers: {
         "Content-Type": "application/json"
         }
     };
-    var s3 = s3URI;
+    var s3 = s3URI; /*configured in your eventing settings*/
     log("s3 url->");
     log(s3);
     log("request variable->");
     log(request);
 
+    /*curl to S3 bucket*/
     var response = curl("PUT", s3URI, request);
     log("response object->")
     log(response);
+    /*if a good response is returned from the S3 call*/
     if(response.status != 200) {
         log("exception found->")
         exceptionBucket[meta.id] = doc;
 
     }
-
-}
-
-function OnDelete(meta, options) {
-    log("Doc deleted/expired", meta.id);
 }
